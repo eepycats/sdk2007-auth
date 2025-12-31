34bb83cfb7eedb43642fb3ab8cbb1e6d1f7cdfda
bt <99887872+caatge@users.noreply.github.com>
2025-12-31 08:36:33 +0100

    rewrite malformed proton tickets + clang format

--- vtable.c
+++ vtable.c
@@ -1,104 +1,177 @@
-#include "windows.h"
-#include "MinHook.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
 
-typedef int (__fastcall *InitiateConnection_t)(
-		void* thisptr,
-		void* edx,
-        int a2,
-        int a3,
-        unsigned int netlong,
-        int a5,
-        int a6,
-        unsigned int a7,
-        int a8,
-        char* ticket,
-        int size);
-typedef void* (*CreateInterfaceFn)(const char* name, int*);
+#include "MinHook.h"
+#include "windows.h"
+
+// #define PRINT_DEBUG_STUFF
+
+#pragma pack(push, 1)
+
+typedef struct id_gcc32 {
+    uint16_t inst;
+    char pad[2];
+    uint32_t lo;
+    uint32_t hi;
+} id_gcc32_t;
+
+typedef struct steam2ticket_gcc32  // sizeof=0x18
+{
+    unsigned int version;
+    id_gcc32_t userid;
+    unsigned int publicip;
+    unsigned int handle;
+} steam2ticket_gcc32_t;
+static_assert(sizeof(steam2ticket_gcc32_t) == 0x18);
+
+typedef struct id_win32 {
+    uint16_t inst;
+    char pad[6];
+    uint32_t lo;
+    uint32_t hi;
+} id_win32_t;
+
+typedef struct steam2ticket_win32 {
+    unsigned int version;
+    char pad[4];
+    id_win32_t userid;
+    unsigned int publicip;
+    unsigned int handle;
+} steam2ticket_win32_t;
+static_assert(sizeof(steam2ticket_win32_t) == 0x20);
+
+#pragma pack(pop)
+
+typedef int(__fastcall *InitiateConnection_t)(void *thisptr, void *edx,
+                                              uintptr_t a2, int a3,
+                                              unsigned int netlong, int a5,
+                                              int a6, unsigned int a7, int a8,
+                                              char *ticket, int size);
+typedef void *(*CreateInterfaceFn)(const char *name, int *);
+typedef void (*Msg_t)(const char *fmt, ...);
+
 InitiateConnection_t ogInitConn;
+Msg_t Msg;
 static int new_vstdlib = 0;
 
-int __fastcall InitiateConnection_hook(
-        void* thisptr,
-		void* edx,
-        int a2,
-        int a3,
-        unsigned int netlong,
-        int a5,
-        int a6,
-        unsigned int a7,
-        int a8,
-        char* enckey,
-        int enckeysize){
-	int len = ogInitConn(thisptr, 0, a2,a3,netlong,a5,a6,a7,a8,enckey,enckeysize);
-	if (len == 60) {
-		return len;
-	} else {
-		return len-8;
-	}
+size_t hexstr(const void *data, size_t len, char *out, size_t out_size) {
+    static const char hex[] = "0123456789abcdef";
+
+    size_t needed = len * 2;
+    if (out_size < needed + 1) {
+        return 0;
+    }
+
+    const uint8_t *cur = data;
+    for (size_t i = 0; i < len; i++) {
+        out[i * 2] = hex[cur[i] >> 0x4];
+        out[i * 2 + 1] = hex[cur[i] & 0x0f];
+    }
+
+    out[needed] = '\0';
+    return needed;
 }
 
+int __fastcall InitiateConnection_hook(void *thisptr, void *edx, uintptr_t a2,
+                                       int a3, unsigned int netlong, int a5,
+                                       int a6, unsigned int a7, int a8,
+                                       char *enckey, int enckeysize) {
+    int len = ogInitConn(thisptr, 0, a2, a3, netlong, a5, a6, a7, a8, enckey,
+                         enckeysize);
 
+#ifdef PRINT_DEBUG_STUFF
+    char hexticket[4096 + 1];  // max ticket + 1
+    hexstr((void *)a2, len, hexticket, sizeof(hexticket));
+    Msg("length %i ticket %s\n", len, hexticket);
+#endif
 
-char __fastcall Load(void* this, void* edx, void* f1, void* f2) {	
-	MH_Initialize();
-	void* vstdlib = GetModuleHandle(L"vstdlib.dll");
-	void* engine = GetModuleHandle(L"engine.dll");
-	if (!vstdlib || !engine){
-		return 0;
-	}
-	
-	CreateInterfaceFn vstdlib_factory = (CreateInterfaceFn)GetProcAddress(vstdlib, "CreateInterface");
+    if (len == 52) { // malformed proton ticket
+        steam2ticket_win32_t newticket = {0};
+        steam2ticket_gcc32_t *oldticket = (steam2ticket_gcc32_t *)(a2 + 28);
+        newticket.version = oldticket->version;
+        newticket.userid.inst = oldticket->userid.inst;
+        newticket.userid.lo = oldticket->userid.lo;
+        newticket.userid.hi = oldticket->userid.hi;
+        newticket.publicip = oldticket->publicip;
+        newticket.handle = oldticket->handle;
+        memcpy(oldticket, &newticket, sizeof(newticket));
+        return 60;
+    }
+    if (len == 60) {
+        return len;
+    } else {
+        return len - 8;
+    }
+}
 
-	char iname[128];
-	int current_version = 1;
-	void* found = NULL;
+char __fastcall Load(void *this, void *edx, void *f1, void *f2) {
+    MH_Initialize();
+    void *vstdlib = GetModuleHandle(L"vstdlib.dll");
+    void *engine = GetModuleHandle(L"engine.dll");
+    void *tier0 = GetModuleHandle(L"tier0.dll");
+    if (!vstdlib || !engine || !tier0) {
+        return 0;
+    }
 
-	for (;;) {
-		sprintf_s(iname, sizeof(iname), "VEngineCvar%03d", current_version);
+    CreateInterfaceFn vstdlib_factory =
+        (CreateInterfaceFn)GetProcAddress(vstdlib, "CreateInterface");
+    Msg = (Msg_t)GetProcAddress(tier0, "Msg");
 
-		void* _current = vstdlib_factory(iname, NULL);
-		if (found && !_current) {
-			current_version--;
-			break;
-		}
-		current_version++;
-		found = _current;
-	}
+    char iname[128];
+    int current_version = 1;
+    void *found = NULL;
 
-	if (current_version > 3) {
-		new_vstdlib = 1;
-	}
-	MH_CreateHook((void*) ( (uintptr_t)engine + 0x0D6780), (void*)InitiateConnection_hook, (void**)&ogInitConn);
-	MH_EnableHook(MH_ALL_HOOKS);
-	return 1;
+    for (;;) {
+        sprintf_s(iname, sizeof(iname), "VEngineCvar%03d", current_version);
+
+        void *_current = vstdlib_factory(iname, NULL);
+        if (found && !_current) {
+            current_version--;
+            break;
+        }
+        current_version++;
+        found = _current;
+    }
+
+    if (current_version > 3) {
+        new_vstdlib = 1;
+    }
+    MH_CreateHook((void *)((uintptr_t)engine + 0x0D6780),
+                  (void *)InitiateConnection_hook, (void **)&ogInitConn);
+    MH_EnableHook(MH_ALL_HOOKS);
+    return 1;
 }
 
 void Unload() {
-	MH_Uninitialize();
-	return;
+    MH_Uninitialize();
+    return;
 }
 
 void Pause() {}
 void UnPause() {}
 
-const char* GetPluginDescription() {
-	return "sdk2007 auth hook";
-}
+const char *GetPluginDescription() { return "sdk2007 auth hook"; }
 
-void __stdcall LevelInit(char const* pMapName) {}
-void __stdcall ServerActivate(void* pEdictList, int edictCount, int clientMax) {}
+void __stdcall LevelInit(char const *pMapName) {}
+void __stdcall ServerActivate(void *pEdictList, int edictCount, int clientMax) {
+}
 void __stdcall GameFrame(char simulating) {}
 void __stdcall LevelShutdown(void) {}
-void __stdcall ClientActive(void* pEntity) {}
-void __stdcall ClientDisconnect(void* pEntity) {}
-void __stdcall ClientPutInServer(void* pEntity, char const* playername) {}
+void __stdcall ClientActive(void *pEntity) {}
+void __stdcall ClientDisconnect(void *pEntity) {}
+void __stdcall ClientPutInServer(void *pEntity, char const *playername) {}
 void __stdcall SetCommandClient(int index) {}
-void __stdcall ClientSettingsChanged(void* pEdict) {}
+void __stdcall ClientSettingsChanged(void *pEdict) {}
 
-int __stdcall ClientConnect(void* bAllowConnect, void* pEntity, const char* pszName, const char* pszAddress, char* reject, int maxrejectlen) { return 0; }
+int __stdcall ClientConnect(void *bAllowConnect, void *pEntity,
+                            const char *pszName, const char *pszAddress,
+                            char *reject, int maxrejectlen) {
+    return 0;
+}
 
 void ClientCommand() {
-	__asm {
+    __asm {
 		mov eax, [new_vstdlib]
 
 		cmp eax, 0
@@ -109,41 +182,46 @@ void ClientCommand() {
 		old_interface :
 		xor eax, eax
 		ret 4
-	}
+    }
 }
 
-int __stdcall  NetworkIDValidated(const char* pszUserName, const char* pszNetworkID) { return 0; };
-void __stdcall OnQueryCvarValueFinished(int iCookie, void* pPlayerEntity, int eStatus, const char* pCvarName, const char* pCvarValue) {};
-void __stdcall OnEdictAllocated(void* edict) {};
-void __stdcall OnEdictFreed(const void* edict) {};
-
-static void* vtable[] = {
-	Load,
-	Unload,
-	Pause,
-	UnPause,
-	GetPluginDescription,
-	LevelInit,
-	ServerActivate,
-	GameFrame,
-	LevelShutdown,
-	ClientActive,
-	ClientDisconnect,
-	ClientPutInServer,
-	SetCommandClient,
-	ClientSettingsChanged,
-	ClientConnect,
-	ClientCommand,
-	NetworkIDValidated,
-	OnQueryCvarValueFinished,
-	OnEdictAllocated,
-	OnEdictFreed
+int __stdcall NetworkIDValidated(const char *pszUserName,
+                                 const char *pszNetworkID) {
+    return 0;
 };
+void __stdcall OnQueryCvarValueFinished(int iCookie, void *pPlayerEntity,
+                                        int eStatus, const char *pCvarName,
+                                        const char *pCvarValue){};
+void __stdcall OnEdictAllocated(void *edict){};
+void __stdcall OnEdictFreed(const void *edict){};
 
-void* pVtable = &vtable;
-void* ppVtable = &pVtable;
+static void *vtable[] = {Load,
+                         Unload,
+                         Pause,
+                         UnPause,
+                         GetPluginDescription,
+                         LevelInit,
+                         ServerActivate,
+                         GameFrame,
+                         LevelShutdown,
+                         ClientActive,
+                         ClientDisconnect,
+                         ClientPutInServer,
+                         SetCommandClient,
+                         ClientSettingsChanged,
+                         ClientConnect,
+                         ClientCommand,
+                         NetworkIDValidated,
+                         OnQueryCvarValueFinished,
+                         OnEdictAllocated,
+                         OnEdictFreed};
 
-__declspec(dllexport) void* CreateInterface(const char* name, int* _) {
-	if (strstr(name, "ISERVERPLUGINCALLBACKS")) return ppVtable;
-	else return NULL;
+void *pVtable = &vtable;
+void *ppVtable = &pVtable;
+
+__declspec(dllexport) void *CreateInterface(const char *name, int *_) {
+    if (strstr(name, "ISERVERPLUGINCALLBACKS"))
+        return ppVtable;
+    else
+        return NULL;
 }
--- xmake.lua
+++ xmake.lua
@@ -3,14 +3,14 @@ target("steamhook")
 	add_defines("UNICODE")	
     set_arch("x86")
     set_kind("shared")
-    set_languages("c11")
+    set_languages("c23")
     set_symbols("debug")
-	
+
 	-- minhook
 	add_files("vendor/minhook/src/*.c")
 	add_files("vendor/minhook/src/hde/hde32.c")
 	add_includedirs("vendor/minhook/include/", "vendor/minhook/src")
-	
+
     add_files("*.c")
 	
     after_build(function (target)
